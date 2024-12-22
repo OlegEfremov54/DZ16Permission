64b6edbc03fa9812b9c4d797cc62eb73d1af11d6
OlegEfremov54 <ole_f@mail.ru>
2024-12-22 20:57:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dz16permission/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.dz16permission
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.dz16permission.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var toolbarShop = binding.toolbarMain
+        setSupportActionBar(toolbarShop)
+        title = " Открытый доступ"
+        toolbarShop.subtitle = "  Версия 1.Главная страница"
+        toolbarShop.setLogo(R.drawable.pleer)
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.infoMenuMain -> {
+                Toast.makeText(applicationContext, "Автор Ефремов О.В. Создан 22.12.2024",
+                    Toast.LENGTH_LONG).show()
+            }
+            R.id.exitMenuMain ->{
+                Toast.makeText(applicationContext, "Работа приложения завершена",
+                    Toast.LENGTH_LONG).show()
+                finishAffinity()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
